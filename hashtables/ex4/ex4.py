6b8edf8921504fccf2547f6fc236368e56e94e77
Amy Beisel <amy.beisel@gmail.com>
2020-09-11 20:58:34 -0600

    completed

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,10 +2,25 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    
+    #set up variables
+    #hash table dictionary
+    cache = {}
+    #result list
+    result = [] 
+    
 
+    #loop through the list
+    for nums in a:
+        cache[nums] = nums
+        
+        #if number not 0 or in dictionary. 
+        if nums != 0 and - nums in cache: 
+            #return number as positive (absolute number) and append in list
+            result.append(abs(nums))
     return result
 
 
 if __name__ == "__main__":
+
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
