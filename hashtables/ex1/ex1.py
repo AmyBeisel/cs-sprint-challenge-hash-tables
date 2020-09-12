9962f4ae47f99b9f67d9c3a778b46aae3bd2280a
Amy Beisel <amy.beisel@gmail.com>
2020-09-11 19:51:22 -0600

    Completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,29 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
+
+def get_indices_of_item_weights(weights, length, limit):
+  
+    #set cache
+    cache = {}
+    
+    # Checks list of weights by index
+    for i in range(length):
+        #store each weight as a key
+        weight = weights[i]
+        #print(f'key and value:{i, weight}')
+        if weight in cache:
+            #store the weight index as the value
+            value = cache[weight]
+            #return tuple
+            return(i, value)
+            
+        
+        #calculate the difference
+        difference = limit - weight
+        #add to cache
+        cache[difference] = i
+        #print(difference)
     return None
+
+
+if __name__ == "__main__":
+    print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
